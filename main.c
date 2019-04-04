5841e9caa29d768da9c9e376e4383792e382bbfb
Nick O'Brien <c3329907@uon.edu.au>
2019-04-04 07:33:50 +0000

    Menu progress

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main () {
+int main () 
+{
     
-
+    char rotation;
+    char substitution; 
+    char method[2] = {rotation, substitution};
+    
+    printf("Do you want to use: \n rotation cipher? \n OR \n substitution cipher? \n Input either rotation or substitution \n");
+    scanf("%s", method);
+        
+    printf("%s was chosen, do you want to encrypt or decrypt?\n type either encrypt or decrypt", method);
     return 0;    
 }
