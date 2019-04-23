77e917fb8ff0e9107ec2cc7245f80c9569ae5578
Nick O'Brien <c3329907@uon.edu.au>
2019-04-23 11:41:30 +0000

    Began creating the rotation cipher with encryption function.

--- main.c
+++ main.c
@@ -1,15 +1,42 @@
-#include <stdio.h>
+/*
 
-int main () 
-{
+Title: ENG1003 Project 1 - Rotation & Substitution Cipher
+Student: Nicholas O'Brien 
+Student Number: 3329907
+
+HOW TO USE THIS PROGRAM:
+
+
+*/
+
+#include <stdio.h> // fget(), stdin
+#include <string.h> //stringlen() function
+#include <stdlib.h>
+
+//Define the Rotation cipher function for encrytion of type char.
+void rotationEncrypt(char* message, int key);
+
+
+int main(void) {
     
-    char rotation;
-    char substitution; 
-    char method[2] = {rotation, substitution};
+    int key = 1;
+    int x;
+    char message[];
     
-    printf("Do you want to use: \n rotation cipher? \n OR \n substitution cipher? \n Input either rotation or substitution \n");
-    scanf("%s", method);
-        
-    printf("%s was chosen, do you want to encrypt or decrypt?\n type either encrypt or decrypt", method);
-    return 0;    
+    printf("\nPlease enter a string:\t"); //Ask the user for the message they wish to use.
+    fgets(message, sizeof(message), stdin); //Gets the user's text input and stores it in the message variable.
+}
+
+void rotationEncrypt(char* message, int key) {
+    
+    int i;
+    char cipher; //this is the encryted message
+    /*
+    while(message[i] != '\0' && strlen(message)-1 > i){
+		cypherValue = ((int)message[i] -97 + key) % 26 + 97;
+		cypher = (char)(cypherValue);
+	*/
+    for(i = 0; (message[i] != '\0' && strlen(message)-1 > i); i++) {
+        cypher = ((int)message[i])
+    }
 }
